e4341c45a948d68a6670eea0758fdc879aa43ce5
cspags <cspags@gmail.com>
2013-06-04 00:33:47 -0500

    Finish user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   #verbose version
   #validates(:name, { presence: true, length: { maximum: 50 } })
@@ -25,4 +26,10 @@ class User < ActiveRecord::Base
   					uniqueness: { case_sensitive: false }
   validates :password, presence: true
   validates :password_confirmation, presence: true, length: { minimum: 6 }
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
